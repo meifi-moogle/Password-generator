303b9d4194f19c125248ee286a6198ac83d9efb2
ploop <106459637+meifi-moogle@users.noreply.github.com>
2023-02-25 06:32:46 +0100

    Make the generator functional and adjust basic html/css

--- app.js
+++ app.js
@@ -1,21 +1,44 @@
-const btn = document.querySelector('#btn');
+const password = document.querySelector('.password');
+const checkbox = document.querySelector('#checkbox');
 
-//~!@#$%^&*+-/|.,{}[]();:
-
-
-function makePassword1(length) {
-    const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let result = '';
-
-    for ( let i = 0; i < length; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() * characters.length));
+function checkboxCheck() {          //change characters according to checkbox
+    if (checkbox.checked === true) {
+        return 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*+-/|.,{}[]();:';
+    } else {
+        return 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     }
-    return result;
 }
 
-btn.addEventListener('click', function() {
-    
-console.log(makePassword1(12));
+const select = document.querySelector('#numberSelect');
+
+function numberCheck() {            //use number value
+    number = select.value;
+    return number;
+}
+
+function makePassword(length) {     //make password based on settings
+    let result = '';
+    let characters = checkboxCheck();
+    length = numberCheck();
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    password.textContent = result;
+}
+
+const copyBtn = document.querySelector('.copy-btn');
+
+copyBtn.addEventListener('click', function () {      //copy to clipboard
+    let text = password.textContent;
+    navigator.clipboard.writeText(`${text}`);
+    alert('Password ready to paste!')
 })
 
+const restartBtn = document.querySelector('.restart-btn')
+
+restartBtn.addEventListener('click', () => {
+    makePassword();
+})
+
+makePassword();
 
